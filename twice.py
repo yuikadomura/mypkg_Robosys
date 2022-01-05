facd8fe27fa971d3f3436d80b2444e74e2a9097d
Yui Kadomura <yui314jumping@gmail.com>
2022-01-05 18:40:52 +0000

    Update

--- twice.py
+++ twice.py
@@ -5,10 +5,17 @@
 import rospy
 from std_msgs.msg import Int32
 
+n = 0
+
 def cb(message):
-    rospy.loginfo(message.data*2)
+    global n
+    n = message.data*2
 
 if __name__ == '__main__':
     rospy.init_node('twice')
     sub = rospy.Subscriber('count_up', Int32, cb)
-    rospy.spin()
+    pub = rospy.Publisher('twice', Int32, queue_size=1) 
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        pub.publish(n)
+        rate.sleep()
